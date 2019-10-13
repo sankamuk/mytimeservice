7cb270d49d3b5c65e303735fa92d48ba72d7b6d0
Sankar Mukherjee <sanmuk21@gmail.com>
2019-10-14 01:50:18 +0200

    Add files via upload

--- test_app.py
+++ test_app.py
@@ -1,6 +1,7 @@
 import pytest
 import app
-import os.path
+import base64
+import os
 
 def test_auth():
   assert app.authmod('sankar', 'Password123') == True
@@ -14,3 +15,10 @@ def test_hitrecorder():
     perm_store = '/storage/hitcount.txt'
   with open(perm_store, 'r') as f:
     assert (len(f.read().split("\n")) - 1) == hits
+
+def test_app():
+  response = app.app.test_client().get('/time')
+  assert response.status_code == 403
+  valid_credentials = base64.b64encode(b'sankar:Password123').decode('utf-8')
+  response = app.app.test_client().get('/time', headers={'Authorization': 'Basic ' + valid_credentials})
+  assert response.status_code == 200
